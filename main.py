1e4ea28f6e10aa95acffc9825c04cbe6d5ba7fd7
antunez0425 <antunez_ariel@yahoo.com>
2024-12-11 21:54:54 -0500

    Added movement to player, created asteroid.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -7,14 +9,32 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color('black'))
+
+
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+
+
 
 if __name__ == "__main__":
     main()
